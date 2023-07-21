de62cfc448bf4a759884c2a0e8a2d31c52e8c3d5
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-21 10:29:07 -0600

    feat: create food objects

--- app/poros/food.rb
+++ app/poros/food.rb
@@ -1,3 +1,13 @@
 class Food
-  
+  attr_reader :code,
+              :description,
+              :ingredients,
+              :owner
+
+  def initialize(data)
+    @code         = data[:fdcId]
+    @description  = data[:description]
+    @ingredients  = data[:ingredients]
+    @owner        = data[:brandOwner]
+  end
 end
