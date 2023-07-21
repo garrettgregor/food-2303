bdcd9d7562f787c1e3697f4d0129c927fdaee4d5
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-21 10:32:56 -0600

    test: add foods page and search to root page

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe '/foods', type: :feature do
+  describe 'as a visitor' do
+    it 'has information about the food that was searched for', :vcr do
+      visit root_path
+
+      fill_in(:q, with: 'sweet potatoes')
+      click_on('Search')
+
+      expect(current_path).to eq(foods_path)
+      expect(page).to have_content('Total # of SWEET POTATOES: 55579')
+      expect(page).to have_content('GTIN/UPC Code: 983525')
+      expect(page).to have_content('Description: SWEET POTATOES')
+      expect(page).to have_content('Ingredients: ORGANIC SWEET POTATOES.')
+      expect(page).to have_content('Brand Owner: NOT A BRANDED ITEM')
+    end
+  end
+end
